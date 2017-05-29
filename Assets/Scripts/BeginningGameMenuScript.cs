7e3ba928e7bcb64f48f294b01b18e7d301872553
Nils Erickson <nils.o.erickson@gmail.com>
2017-05-28 21:30:28 -0700

    Asset Upload
    Contains assets of Unity project

---
+++ Assets/Scripts/BeginningGameMenuScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeginningGameMenuScript : MonoBehaviour {
+
+		void OnGUI()
+	{
+		const int buttonWidth = 120;
+		const int buttonHeight = 60;
+		
+		if (
+			GUI.Button(
+			// Center in X, 1/3 of the height in Y
+			new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
+			(1 * Screen.height / 3) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+			),
+			"Begin!"
+			)
+			)
+		{
+			// Reload the level
+			Application.LoadLevel("Level1");
+		}
+	}
+}
